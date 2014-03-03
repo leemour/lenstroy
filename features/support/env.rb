7678add757d1521e1fa4a7595d263ffe15523202
leemour <leemour@gmail.com>
2014-03-03 16:57:13 +0400

    Initial commit

---
+++ features/support/env.rb
@@ -0,0 +1,10 @@
+RACK_ENV = 'test' unless defined?(RACK_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../../config/boot")
+
+require 'capybara/cucumber'
+require 'rspec/expectations'
+
+##
+# You can handle all padrino applications using instead:
+#   Padrino.application
+Capybara.app = Lenstroy2::App.tap { |app|  }
