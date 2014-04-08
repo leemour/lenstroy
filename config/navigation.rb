db943626be54fc780aea5d1b05cc910541d688a5
leemour <leemour@gmail.com>
2014-04-08 19:18:51 +0400

    Change Navigation paths, try to fix NewRelic

--- config/navigation.rb
+++ config/navigation.rb
@@ -8,28 +8,28 @@ SimpleNavigation::Configuration.run do |navigation|
     end
     primary.item :houses,       'Загородные дома',      '/houses' do |houses|
       houses.item :brus,        'Из бруса',             '/houses/brus'
-      houses.item :karkass,     'Каркасные дома',       '/houses/karkass'
+      houses.item :karkass,     'Каркасные дома',       '/houses/karkas'
       houses.item :gazobeton,   'Дома из газобетона',   '/houses/gazobeton'
       houses.item :srub,        'Срубы, бани',          '/houses/srub'
     end
     primary.item :works,        'Виды работ',           '/works' do |works|
-      works.item :krovlia,      'Кровельные работы',    '/works/krovel'
+      works.item :krovlia,      'Кровельные работы',    '/works/roof'
       works.item :fasad,        'Фасадные работы',      '/works/fasad'
-      works.item :otdelka,      'Внутренняя отедка',    '/works/otdelka'
-      works.item :zabor,        'Заборы',               '/works/zabor'
-      works.item :fundament,    'Фундамент',            '/works/fundament'
+      works.item :otdelka,      'Внутренняя отделка',   '/works/otdelka'
+      works.item :zabor,        'Заборы',               '/works/fences'
+      works.item :fundament,    'Фундамент',            '/works/foundation'
     end
-    primary.item :repair,       'Ремонт квартир/офисов','/repair' do |repair|
-      repair.item :design,      'Дизайн интерьера',     '/repair/design'
-      repair.item :otdelka,     'Отделочные работы',    '/repair/fasad'
-      repair.item :otdelka,     'Перепланировка',       '/repair/otdelka'
+    primary.item :repair,       'Ремонт квартир/офисов','/renovation' do |renovation|
+      renovation.item :otdelka, 'Отделочные работы',    '/renovation/finishing'
+      renovation.item :otdelka, 'Перепланировка',       '/renovation/replanning'
     end
     primary.item :projects,     'Проектирование',       '/projects' do |projects|
-      projects.item :dom,       'Проекты домов',        '/projects/dom'
-      projects.item :kvartira,  'Проекты квартир',      '/projects/kvartira'
-      projects.item :bania,     'Проекты бань',         '/projects/bania'
+      projects.item :dom,       'Проекты домов',        '/projects/houses'
+      projects.item :kvartira,  'Проекты квартир',      '/projects/flats'
+      projects.item :bania,     'Проекты бань',         '/projects/bath-houses'
     end
-    primary.item :gallery,      'Галерея',              '/gallery'
+    primary.item :design,       'Дизайн интерьера',      '/design'
+    # primary.item :gallery,      'Галерея',              '/gallery'
 
 
     primary.item :promotions,    'Акции',          '/promotions' do |promotions|
